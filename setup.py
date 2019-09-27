2fd095d450cc49cb21eb656307014d3bd434fcdc
Your Name <you@example.com>
2019-09-27 10:50:09 +0200

    second commit

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ REQUIRED_PACKAGES = []
 PACKAGE_NAME = 'modules_dataflow_job_test'
 PACKAGE_VERSION = '0.0.1'
 setuptools.setup(
-    name='modules',
-    version=1.0,
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
     description='Example project',
     install_requires=REQUIRED_PACKAGES,
     packages=setuptools.find_packages(),
